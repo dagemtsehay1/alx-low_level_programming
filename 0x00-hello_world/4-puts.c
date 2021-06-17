53d795926414de80153d38c983a276fab96f925b
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-06-17 04:42:07 -0700

    Update 4-puts.c
    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
+ * main - Entry point
  *
- * Return always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzel");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
