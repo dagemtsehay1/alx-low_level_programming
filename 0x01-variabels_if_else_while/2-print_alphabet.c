186c390217830ad433010df7b1cea3dfbabf9504
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-18 03:20:30 -0700

    print alphabet

---
+++ 0x01-variabels_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main function
+ */
+
+int main(void)
+{
+  int num;
+  num = 97;
+
+  while (num <= 122)
+    {
+      putchar (num);
+      num++;
+    }
+  putchar('\n');
+  return (0);
+}
