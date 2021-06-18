0a211dbbc79dd0ebe90be44b7609f97bd9a7411e
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-06-18 19:38:03 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int r;
+
+for (r = 0; r <= 9; r++)
+{
+printf("%d", r);
+}
+printf("\n");
+return (0);
+}
