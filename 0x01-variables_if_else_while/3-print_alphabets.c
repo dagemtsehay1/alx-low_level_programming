ab1f0c0fc17da0bc54ce97fb3e95d5c2c39f287d
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-06-18 19:35:42 +0300

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 /**
- * main function
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-  int num;
-  num = 97;
-  while (num <= 122)
-    {
-      putchar (num);
-      num++;
-    }
-  num = 65;
-  while (num <= 90)
-    {
-      putchar (num);
-      num++;
-    }
-  putchar ('\n');
+int x, y;
 
-  return (0);
+<<<<<<< HEAD
+for (m = 97; m <= 122; m++)
+putchar(m);
+for (n = 65; n <= 90; n++)
+putchar(n);
+=======
+for (x = 97; x<= 122; x++)
+{
+putchar(x);
+}
+for (y = 65; y <= 90; y++)
+{
+putchar(y);
+}
+>>>>>>> c38d008b18676f16774155151031a0fcc1c46752
+putchar('\n');
+return (0);
 }
