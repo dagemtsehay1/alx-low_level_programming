90b6ac5be1cd10c72cef391b888eed8991747c35
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-06-18 19:41:13 +0300

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int z;
+
+for (z = 48; z <= 57; z++)
+{
+putchar(z);
+}
+for (z = 97; z <= 102;z++)
+{
+putchar(z)
+}
+putchar('\n');
+return (0);
+}
