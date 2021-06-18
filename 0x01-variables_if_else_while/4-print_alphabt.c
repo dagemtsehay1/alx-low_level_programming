518201a1a0e181ac09ad519820367cf781a5cc61
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-18 10:13:34 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,22 +6,15 @@
  */
 int main(void)
 {
-int y;
-
-for (y = 97; y <= 122; y++)
-{
-<<<<<<< HEAD
-       if (m == 101 || m == 113)
-=======
-if (y == 101 || y == 113)
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-{
-}
-else
-{
-putchar(y);
-}
-}
-putchar('\n');
-return (0);
+  int x;
+  x = 97;
+  while (x <= 122)
+    {
+      if (x == 113 || x == 101)
+	continue;
+      putchar (x);
+      x++;
+    }
+  putchar('\n');
+  return (0);
 }
