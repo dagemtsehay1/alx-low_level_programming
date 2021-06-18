a49b2d382c6f9d36b729115bd5405065769a532e
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-18 10:08:26 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main function
  */
 int main(void)
 {
-int x, y;
+  int x;
 
-<<<<<<< HEAD
-for (m = 97; m <= 122; m++)
-putchar(m);
-for (n = 65; n <= 90; n++)
-putchar(n);
-=======
-for (x = 97; x<= 122; x++)
-{
-putchar(x);
-}
-for (y = 65; y <= 90; y++)
-{
-putchar(y);
-}
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-putchar('\n');
-return (0);
+  x = 97;
+  while (x <= 122)
+    {
+      putchar (x);
+      x++;
+    }
+  x = 65;
+    while (x <= 90)
+      {
+       putchar (x);
+       x++;
+      }
+  putchar ('\n');
+  return (0);
 }
