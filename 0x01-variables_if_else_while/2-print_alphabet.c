d301996d58f1f61aa40a7c683444d2564f16d6c4
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-18 09:51:16 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,21 +6,13 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
-int num;
-num = 97;
-while (num <= 122)
-putchar(num);
-num++;
-=======
-int p;
-p = 97;
-while (p <= 122)
-{
-putchar(p);
-p++;
-}
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-putchar('\n');
-return (0);
+  int num;
+  num = 97;
+  while (num <= 122)
+    {
+      putchar(num);
+      num++;
+    }
+  putchar('\n');
+  return (0);
 }
