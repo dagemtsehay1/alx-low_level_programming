39975a5012ae281110372bb83ab5d8c43685e336
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-06-22 12:23:13 +0300

    holberton header

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,32 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+void fibonacci(void);
+
+int _putchar(char c);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+void jack_bauer(void);
+
+int print_last_digit(int);
+
+void print_times_table(int n);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+#endif
