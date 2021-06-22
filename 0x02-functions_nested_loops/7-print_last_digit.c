8bba27ad4a77a1db03823fdcfd4cdb98e2c80dda
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-22 02:57:04 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,10 @@
 
 int print_last_digit(int n)
 {
-  n = n % 10;
+n = n % 10;
 
-  if (n < 0)
-    n = -n;
-  _putchar(n + '0');
-  return (n);
+if (n < 0)
+n = -n;
+_putchar(n + '0');
+return (n);
 }
