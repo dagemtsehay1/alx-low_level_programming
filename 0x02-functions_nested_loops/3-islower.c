7565593246c117de7b7d4ba28a916dd794ae965d
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-22 01:19:50 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _islower - checks for a lowercase character
+ * @c: letter being tested
+ * Return : Always 0
+ */
+
+int _islower(int c)
+{
+  if (c >= 97 && c <= 122)
+    {
+      return (1);
+    }
+  return (0);
+}
