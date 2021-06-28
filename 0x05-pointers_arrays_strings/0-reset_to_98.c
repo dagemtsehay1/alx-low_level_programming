ed62a89c2212f36723c4f1c8e79a886efffc048e
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-06-28 14:17:13 +0300

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
+
 /**
- * reset_to_98 - a function that change the value of n
- * 
- * Return: Always 0
+ * reset_to_98 - main
+ * @n: - pinter to int
  */
+
 void reset_to_98(int *n)
 {
 *n = 98;
-return (0);
 }
