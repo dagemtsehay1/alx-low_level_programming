13db9235bb2ff82725dcd1b8a08cf6ab5284ef7d
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-28 04:07:44 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - a function that change the value of n
+ * 
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+  *n = 98;
+  return 0;
+}
