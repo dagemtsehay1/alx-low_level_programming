70ab9d5785b856bee853588bb26ec7f22a66bb35
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-28 05:36:01 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-
 /**
  * puts2 - prints one char out of 2
  * @str: type
  *
  */
-
 void puts2(char *str)
 {
-	int i = 0;
-	while (str[i] != '\0')
-	{
-	  _putchar(str[i]);
-	  i += 2;
-	}
+int i = 0;
 
-	_putchar('\n');
+while (str[i] != '0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
 }
