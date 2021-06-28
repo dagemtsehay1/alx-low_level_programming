6d4d3cf8a22894aa23f54c42f5f8d3c756eef8e6
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-28 04:42:09 -0700

    get string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,17 @@
 /**
  * _strlen - main
  * @s: pointer
+ * Return: string length
  */
 
 int _strlen(char *s)
 {
-return (strlrn(*s));
+int l;
+int l = 0;
+while (*s != null)
+{
+s++;
+l += 1;
+}
+return (l);
 }
