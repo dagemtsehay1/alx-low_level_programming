5b83287e6339e02b3ddcdd8f626db4071985e949
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-06-28 15:05:04 +0300

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,17 @@
-#include "holberton"
+#include "holberton.h"
+
 /**
- * _puts - main
- * @str: pointer
+ * _puts - prints string
+ * @str: input string
  * Return: no return
  */
 
 void _puts(char *str)
 {
-while (*str != '\0')
-{
-_putchar(*str);
-str++;
-}
-_putchar('\n');
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
