18b24ef525d096440ed13878b8219a27ccafeb77
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-06-28 15:21:46 +0300

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,11 +12,13 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-	  if (i % 2 == 0)
-	    {
-	      _putchar (str[i]);
-	    }
 		i++;
+	}
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j = j + 2;
 	}
 	_putchar('\n');
 }
