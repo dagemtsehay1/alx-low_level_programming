83bc13e13584154ea3fb074243d2575aa0de43ac
Dagem Tsehay <dagemtsehay4@gmail.com>
2021-06-29 07:22:56 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,11 @@
 
 void puts2(char *str)
 {
-	int i, j = 0;
-
-	while (str[i] != '\0')
-	{
-	  _putchar(str[i]);
-	  i += 2;
-	}
-	_putchar('\n');
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
 }
