4dc982b68823cd8bdeae5db2f1fd5b2e38b356fd
dagemtsehay1 <84669128+dagemtsehay1@users.noreply.github.com>
2021-07-15 20:02:45 +0300

    more mallcon free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
+
 /**
  * malloc_checked - allocates memory using malloc
  * @b: bytes allocate
  * Return: pointer
  */
-
 void *malloc_checked(unsigned int b)
 {
-void *ptr;
+	void *ptr;
 
-ptr = malloc(b);
-if (ptr == Null)
-exit(98);
-return (ptr);
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
