f3238c739014ecc767dcfc67952b8ef4e375b4a4
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-08-05 16:14:07 +0300

    bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,23 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * flip_bits - number of bits you would need to flip
- * to get from one number to another
- * @n: parameter
- * @m: destiny
- * Return: nflips
+ * flip_bits - returns the number of bits you would need
+ * to flip to get from one number to another
+ * @n: number
+ * @m: other number
+ * Return: returns the number of bits you would need
+ * to flip to get from one number to another
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int i, nflips = 0;
-	unsigned long int j = sizeof(unsigned long int) * 8;
+	int shift = (sizeof(n) * 8);
+	int dist = 0;
 
-	for (i = 0; i < j; i++)
-	{
-		if ((m & 1) != (n & 1))
-			nflips += 1;
-		n = n >> 1;
-		m = m >> 1;
-	}
-	return (nflips);
+	while (shift--)
+		dist += (n >> shift & 1) != (m >> shift & 1);
+	return (dist);
 }
