7f0e6ab1c20876503115f0aead5caaa154156f78
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-08-05 11:15:41 +0300

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,48 @@
 #include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- * Return: the converted number
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+*/
+
+
+int _strlen(const char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
+}
+
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary number
+ *
+ * Return: 0 or converted number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	unsigned int n = 0;
+	int i, len;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	len = _strlen(b);
+
+	for (i = 0; i != len; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[len - i - 1] == '1')
+			n += 1 << i;
+		else if (b[len - i - 1] != '0')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (dec_val);
+
+	return (n);
 }
