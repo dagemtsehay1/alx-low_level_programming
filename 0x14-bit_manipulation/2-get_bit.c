5773261dbc514445d82b7506c27a5e6a09efcafc
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-08-05 16:10:45 +0300

    bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * get_bit - value of a bit at a given index
- * @n: decimal parameter
- * @index: index
- * Return: val
+ * get_bit - returns the value of a bit at a given index.
+ *
+ * @n: number
+ * @index: bit index
+ *
+ * Return: returns the value of a bit at a given index.
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit;
+	if (index >= sizeof(n) * 8)
+		return (-1);
 
-	bit = (n >> index);
-	if (index > 32)
-	return (-1);
-	return (bit & 1);
+	return ((n >> index) & 1);
 }
