96ae2d6378b02e69ffac5eda5a174ae31880213c
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-08-05 14:11:09 +0300

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,43 @@
-#include "holberton.h"
+#include "main.h"
+
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- * Return: the converted number
+ * binary_to_uint - converts a binary number to an unsigned int.
+ *
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0 if
+ * -there is one or more chars in the string b that is not 0 or 1
+ * -b is NULL
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	int i = 0, m = 1;/*string length*/
+	unsigned int result = 0;
 
+	/*if b is null return 0*/
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	/*loop thru the string*/
+	while (*(b + i) != '\0')
 	{
-		if (b[i] < '0' || b[i] > '1')
+		/*it the current character is not 1 or 0 return 0*/
+		if (*(b + i) != '0' && *(b + i) != '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+
+		i++;
 	}
-	return (dec_val);
+
+	--i;
+
+	while (i >= 0)
+	{
+		result += (*(b + i) - '0') * m;
+		m *= 2;
+		--i;
+	}
+
+	return (result);
 }
