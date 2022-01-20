274339eea233044d13a12fe56c08f828a39fef04
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2022-01-20 09:50:49 +0300

    task 13

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -30,5 +30,6 @@ int exponential_search(int *array, size_t size, int value);
 int binary_search_ex(int *array, int value, int first, int last);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
